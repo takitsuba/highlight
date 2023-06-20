3176a152f348e585398eb375b410d611a6b72172
Hiroo Takizawa <kei.cataract@gmail.com>
2023-06-20 12:17:19 +0900

    specify color by name

--- src/highlight/main.py
+++ src/highlight/main.py
@@ -1,8 +1,11 @@
 import fitz
+from fitz.utils import getColor
+
 
 file = "data/input/Can Large Language Models Be an Alternative to Human Evaluations?.pdf"
 doc = fitz.open(file)
-search_term = "First"
+search_term = "Human"
+
 
 for current_page in range(len(doc)):
     page = doc.load_page(current_page)
@@ -11,6 +14,8 @@ for current_page in range(len(doc)):
         # ref. https://pymupdf.readthedocs.io/en/latest/page.html#Page.add_highlight_annot
         text_quads = page.search_for(search_term, quads=True)
         highlight = page.add_highlight_annot(text_quads)
+        rgb = getColor("PLUM1")
+        highlight.set_colors(stroke=rgb)
         highlight.update()
 
 doc.save("data/output/highlighted.pdf", garbage=4, deflate=True)
